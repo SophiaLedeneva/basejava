ae0abd384d0a1a0a277212a9cffe2509b6db659e
SophiaLedeneva <s.telkova1997@gmail.com>
2021-10-31 15:05:49 +0300

    HW0

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,53 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
+    //
 
     void clear() {
+        for (int index = 0; index < size; index++) {
+                storage[index] = null;
+        }
+        size = 0;
     }
-
+    //+
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
+    //+
     Resume get(String uuid) {
+
+        for (int index = 0; index < size; index++) {
+            if (storage[index].toString().equals(uuid))
+                return storage[index];
+        }
         return null;
     }
 
+    //
     void delete(String uuid) {
-    }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
+        for (int index = 0; index < size; index++) {
+            if (storage[index] == null || storage[index].toString().equals(uuid)) {
+                storage[index] = storage[index + 1];
+                storage[index + 1] = null;
+            }
+        }
+            size--;
+        }
+
+    //
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
-
+//
     int size() {
-        return 0;
+        return size;
     }
 }
